10e68b2d43c7545bf1c3c056252087ef76d6538d
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-12-25 22:10:34 -0500

    Add 66

---
+++ src/66-plus-one/main.rs
@@ -0,0 +1,33 @@
+fn main() {}
+
+#[allow(dead_code)]
+fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+    let len = digits.len();
+    digits[len - 1] += 1;
+
+    for i in (1..digits.len()).rev() {
+        if digits[i] > 9 {
+            digits[i] = 0;
+            digits[i - 1] += 1;
+        }
+    }
+
+    if digits[0] > 9 {
+        digits[0] = 0;
+        digits.insert(0, 1);
+    }
+
+    digits
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(plus_one(vec![9]), vec![1, 0]);
+        assert_eq!(plus_one(vec![1, 9]), vec![2, 0]);
+        assert_eq!(plus_one(vec![9, 9]), vec![1, 0, 0]);
+    }
+}
