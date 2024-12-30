fa9ed681ef96c08a38da4ac10a04a343fb534023
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-12-29 22:30:35 -0500

    Syntactic sugar

--- c++/14-longest-common-prefix/trie.cc
+++ c++/14-longest-common-prefix/trie.cc
@@ -37,23 +37,19 @@ public:
 
 private:
   struct TrieNode {
-    bool is_end;
-    unordered_map<char, TrieNode> children;
+    bool is_end = false;
+    unordered_map<char, TrieNode> children{};
   };
 
   void Push(const string_view str) {
     TrieNode *trie_node = &trie_;
 
     for (size_t i = 0; i < str.length(); ++i) {
-      const char c = str[i];
-
-      if (!trie_node->children.contains(c)) {
-        trie_node->children.insert({c, {.is_end = false, .children{}}});
-      }
-
       if (i == str.length() - 1) {
         trie_node->is_end = true;
       }
+
+      const char c = str[i];
 
       trie_node = &trie_node->children[c];
     }
